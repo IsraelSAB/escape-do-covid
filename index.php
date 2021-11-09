938f9ee7c01c648700bb0a804cc33df1c5f95f2b
IsraelSAB <israel-projetos@hotmail.com>
2021-11-08 22:43:29 -0200

    Edit1

--- index.php
+++ index.php
@@ -10,7 +10,6 @@ include 'functions.php';
 <div class="content">
 	<h2>Home</h2>
 	<p>Welcome to the home Escape do Covid 19!</p>
-	<p>Welcome to Escape do Covid!</p>
 </div>
 
 <?=template_footer()?>
