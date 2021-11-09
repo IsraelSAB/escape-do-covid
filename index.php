eef44bb85fbed3ebddc7e4d0b90ab5c24821fda7
IsraelSAB <israel-projetos@hotmail.com>
2021-11-08 23:31:55 -0200

    Edit titulo

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include 'functions.php';
 
 <div class="content">
 	<h2>Home</h2>
-	<p>Welcome to the home Escape do Covid 19!</p>
+	<p>Bem vindo ao site Escape do Covid!</p>
 </div>
 
 <?=template_footer()?>
